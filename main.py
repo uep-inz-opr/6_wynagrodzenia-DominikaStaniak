9c5777600920c331c4eba587c928cb063c235c95
Dominika <dominika.staniak4@gmail.com>
2022-05-21 22:45:20 +0200

    podejscie piate

--- main.py
+++ main.py
@@ -18,8 +18,14 @@ def skladki(pracownicy):
   for Pracownik in pracownicy:
       skladki_1 = round((Pracownik.brutto * 0.0976) + (Pracownik.brutto * 0.015) + (Pracownik.brutto * 0.0245),2)
       skladki_2= round((Pracownik.brutto * 0.0976) + (Pracownik.brutto * 0.065) + (Pracownik.brutto * 0.0193) + (Pracownik.brutto * 0.0245) + (Pracownik.brutto * 0.001),2)
-      netto = round(Pracownik.brutto - round(skladki_1 + ((Pracownik.brutto - skladki_1)*0.09)  + round(((((Pracownik.brutto - 111.25 - skladki_1)*0.18)-46.33) - ((Pracownik.brutto - skladki_1)*0.0775)),2),2),2)
-      koszt = round(Pracownik.brutto + skladki_2)
+      podst_skladki_zdro = round((pracownik.brutto - skladki_1),2)
+      skladka_zdro_pob= round((podst_skladki_zdro * 0.09),2)
+      skladka_zdro_odl=round((podst_skladki_zdro*0.0775),2)
+      kup = 111.25
+      zal_pod_doch_odl = round((round(((round(Pracownik.brutto - kup - skladki_1))* 0.18),2)-46.33),2)
+      zal_pod_doch_pob = round((zal_pod_doch_odl - skladka_zdro_odl))
+      netto = round((Pracownik.brutto - skladki_1 - skladka_zdro_pob - zal_pod_doch_pob),2)
+      koszt = round((Pracownik.brutto + skladki_2),2)
       suma_skladki = skladki_1 + skladki_2
       print (Pracownik.imie + " " + "{:.2f}".format(netto) + " " +"{:.2f}".format(skladki_2) + " " + "{:.2f}".format(koszt))
       suma_kosztow_pracodawcy += koszt
